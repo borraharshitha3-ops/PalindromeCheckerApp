05dc7fcee41ceab6336eb4a4f60eb9c149f7d556
unknown <borraharshitha3@gmail.com>
2026-03-11 22:16:17 +0530

    UC8: Linked list palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,23 +1,47 @@
-import java.util.Stack;
+class Node{
+    char data;
+    Node next;
+
+    Node(char data){
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
-        Stack<Character> stack = new Stack<>();
+        String word = "level";
+
+        Node head = null;
+        Node tail = null;
 
         for(char c : word.toCharArray()){
-            stack.push(c);
+
+            Node newNode = new Node(c);
+
+            if(head == null){
+                head = newNode;
+                tail = newNode;
+            }else{
+                tail.next = newNode;
+                tail = newNode;
+            }
+
         }
 
+        String original = word;
         String reversed = "";
 
-        while(!stack.isEmpty()){
-            reversed += stack.pop();
+        Node current = head;
+
+        while(current != null){
+            reversed = current.data + reversed;
+            current = current.next;
         }
 
-        if(word.equals(reversed)){
+        if(original.equals(reversed)){
             System.out.println("Palindrome");
         }else{
             System.out.println("Not Palindrome");
