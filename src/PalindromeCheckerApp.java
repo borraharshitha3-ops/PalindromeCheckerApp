fc3cc08ea0ca8b004ed7ca18c2b78df799da4ecc
unknown <borraharshitha3@gmail.com>
2026-03-11 21:53:06 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println(" Application Version: 1.0 ");
+
+
+    }
+}
