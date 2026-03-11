c5390794ff0e31f0bd1cbb42e9595bc8f74c5398
unknown <borraharshitha3@gmail.com>
2026-03-11 22:17:12 +0530

    UC9: Recursion based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,47 +1,23 @@
-class Node{
-    char data;
-    Node next;
-
-    Node(char data){
-        this.data = data;
-        this.next = null;
-    }
-}
-
 public class PalindromeCheckerApp {
+
+    static boolean isPalindrome(String str, int start, int end){
+
+        if(start >= end){
+            return true;
+        }
+
+        if(str.charAt(start) != str.charAt(end)){
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
 
     public static void main(String[] args) {
 
-        String word = "level";
+        String word = "madam";
 
-        Node head = null;
-        Node tail = null;
-
-        for(char c : word.toCharArray()){
-
-            Node newNode = new Node(c);
-
-            if(head == null){
-                head = newNode;
-                tail = newNode;
-            }else{
-                tail.next = newNode;
-                tail = newNode;
-            }
-
-        }
-
-        String original = word;
-        String reversed = "";
-
-        Node current = head;
-
-        while(current != null){
-            reversed = current.data + reversed;
-            current = current.next;
-        }
-
-        if(original.equals(reversed)){
+        if(isPalindrome(word,0,word.length()-1)){
             System.out.println("Palindrome");
         }else{
             System.out.println("Not Palindrome");
